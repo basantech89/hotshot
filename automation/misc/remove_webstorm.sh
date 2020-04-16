c247167c8e05884b92493bf23302d081b998eecf
Basant Soni <basantech89@gmail.com>
2020-04-16 19:05:11 +0530

    Add misc commands

---
+++ automation/misc/remove_webstorm.sh
@@ -0,0 +1,9 @@
+cd ~
+sudo rm -r .WebStorm*
+sudo rm -r WebStorm*
+sudo rm -r ~/.java/.userPrefs
+sudo rm -r WebstormProjects
+cd /luv/apps
+sudo rm -r WebStorm-19*
+sudo rm /usr/bin/webstorm
+sudo rm /usr/local/bin/webstorm
